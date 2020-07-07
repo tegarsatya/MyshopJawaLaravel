f75021dbf9898a985cea36630b67c94a35f96cae
Backend Server <backendserver@gmail.com>
2020-07-07 15:09:36 +0700

    installation livewire ke project myshopjawalaravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    @livewire('counter')
                 </div>
             </div>
         </div>
